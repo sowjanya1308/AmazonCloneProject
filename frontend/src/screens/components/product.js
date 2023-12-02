0326185678ff2afd86a4eb08fa19ebd625fe4362
sowjanya1308 <sowjanyasankara@gmail.com>
2023-12-02 18:08:09 +0530

    description

--- frontend/src/screens/components/product.js
+++ frontend/src/screens/components/product.js
@@ -6,6 +6,7 @@ import watch3 from "../../images/watch3.png";
 import { TiStarFullOutline } from "react-icons/ti";
 import { CiDeliveryTruck } from "react-icons/ci";
 import { GiReturnArrow } from "react-icons/gi";
+import { IoLocationOutline } from "react-icons/io5";
 import Header from "../components/header";
 
 const Product = () => {
@@ -52,32 +53,48 @@ const Product = () => {
                 <div>
                     <div className='pt-9 flex'>
                         <div>
-                        <h1 className=' text-2xl font-medium'>Product Details</h1>
-                        <ul className='pt-3 list-disc'>
-                            <li><b>Color:</b> Green</li>
-                            <li><b>Material Type:</b> Leather</li>
-                            <li><b>Watch Moment Type:</b> Quartz</li>
-                            <li><b>Item Weight: </b>190 Grams</li>
-                            <li><b>Country of origin: </b>India</li>
-                        </ul>
+                            <h1 className=' text-2xl font-medium'>Product Details</h1>
+                            <ul className='pt-3 list-disc'>
+                                <li><b>Color:</b> Green</li>
+                                <li><b>Material Type:</b> Leather</li>
+                                <li><b>Watch Moment Type:</b> Quartz</li>
+                                <li><b>Item Weight: </b>190 Grams</li>
+                                <li><b>Country of origin: </b>India</li>
+                            </ul>
                         </div>
-                        <div className='pt-9 ml-20 flex'>
-                            <div>
-                                <label for='quantity' className='font-medium '>Quantity: </label>
-                                <select id='quantity'>
-                                <option selected>1</option>
-                                <option value="US">2</option>
-                                <option value="CA">3</option>
-                                <option value="FR">4</option>
-                                <option value="DE">5</option>
-                                </select>
-
+                        <div className='flex flex-col'>
+                            <div className='pt-9 ml-20 flex'>
+                                <div>
+                                    <label for='quantity' className='font-medium '>Quantity: </label>
+                                    <select id='quantity'>
+                                    <option selected>1</option>
+                                    <option value="US">2</option>
+                                    <option value="CA">3</option>
+                                    <option value="FR">4</option>
+                                    <option value="DE">5</option>
+                                    </select>
+                                </div>
+                                <div className='ml-6'>
+                                <button type="button" class="text-white bg-gradient-to-r from-pink-400 via-pink-500 to-pink-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-pink-300 dark:focus:ring-pink-800 shadow-lg shadow-pink-500/50 dark:shadow-lg dark:shadow-pink-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Add To Cart</button>
+                                </div>
+                                <div className='ml-6'>
+                                <button type="button" class="text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 shadow-lg shadow-purple-500/50 dark:shadow-lg dark:shadow-purple-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Check Out</button>
+                                </div>
                             </div>
-                            <div className='ml-6'>
-                            <button type="button" class="text-white bg-gradient-to-r from-pink-400 via-pink-500 to-pink-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-pink-300 dark:focus:ring-pink-800 shadow-lg shadow-pink-500/50 dark:shadow-lg dark:shadow-pink-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Add To Cart</button>
-                            </div>
-                            <div className='ml-6'>
-                            <button type="button" class="text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 shadow-lg shadow-purple-500/50 dark:shadow-lg dark:shadow-purple-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Check Out</button>
+                            <div className='pt-5 ml-20'>
+                                <h1 className=' text-2xl font-medium text-green-700'>In Stock</h1>
+                                <div className="flex items-center mt-1">
+                                    <input id="default-checkbox" type="checkbox" value="" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded "/>
+                                    <label for="default-checkbox" className="ms-2 text-sm font-normal text-gray-900"> Free Delivery by 10/12/2023, order within 2hr 35min</label>
+                                </div>
+                                <div className="flex items-center mt-1">
+                                    <input id="default-checkbox" type="checkbox" value="" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded "/>
+                                    <label for="default-checkbox" className="ms-2 text-sm font-normal text-gray-900">Fast Delivery by 6/12/2023</label>
+                                </div>
+                                <div className="flex items-center mt-1">
+                                    <IoLocationOutline/>
+                                    <a href="#" className='font-medium'>Deliver to Sowjanya, 520015</a>
+                                </div>
                             </div>
                         </div>
                     </div>
