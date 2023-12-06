a8130effcda977aa6e915d9cbdeff073e15e2f4b
rahul181203 <rahulkanakam123@gmail.com>
2023-12-06 15:58:52 +0530

    add product

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import Product from './screens/components/product';
 import Addtocart from './screens/components/addtocart';
 import ShoppingCart from './screens/components/shoppingcart';
 import categoryList from './screens/categoryProducts/categoryList';
+import AddProduct from './screens/addProduct/addProduct';
 
 
 
@@ -16,6 +17,7 @@ const App = () => {
       <Route path='/cart' Component={Addtocart}/>
       <Route path='/paymentdetails' Component={ShoppingCart}/>
       <Route path='/category' Component={categoryList}/>
+      <Route path='/add' Component={AddProduct}/>
     </Routes>
   )
 }
