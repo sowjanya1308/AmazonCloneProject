c859e4ce97929d201ebe7a66419b65d71c580492
rahul181203 <rahulkanakam123@gmail.com>
2023-12-25 19:30:34 +0530

    changes product

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,6 @@
 const express = require("express")
 const cors = require('cors')
 const dotenv = require("dotenv")
-// const firebase = require("firebase/app")
 const cookieParser = require("cookie-parser")
 const { sendEmail } = require("./controllers/mail");
 const routes = require("./router/routes")
